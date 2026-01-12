657bfa86a7d6ddbdcc3a6d09e68e5745def0a8d5
Akshat Sharma <akshatsharma@Akshats-MacBook-Air.local>
2026-01-12 20:40:16 +0530

    Fix nested git repo and organize experiments

---
+++ experiment-2/src/App.jsx
@@ -0,0 +1,12 @@
+import ButtonBasic from "/components/Button";
+
+function App() {
+  return (
+    <div>
+      <h2>Material UI Example</h2>
+      <ButtonBasic />
+    </div>
+  );
+}
+
+export default App;
---
+++ experiment-2/src/components/Button.jsx
@@ -0,0 +1,12 @@
+import Button from "@mui/material/Button";
+
+function ButtonBasic() {
+  return (
+    <>
+      <Button variant="contained">Button 1</Button>
+      <Button variant="outlined">Button 2</Button>
+    </>
+  );
+}
+
+export default ButtonBasic;
