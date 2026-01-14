279a8de81c5d5f0d8e7555d9cf4bf90502abdcc9
Akshat Sharma <akshatsharma@Akshats-MacBook-Air.local>
2026-01-14 11:30:16 +0530

    Added routing and responsive Home About Contact components

--- experiment-2/src/components/Button.jsx
+++ experiment-2/src/components/Button.jsx
@@ -1,12 +1,12 @@
-import Button from "@mui/material/Button";
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
-function ButtonBasic() {
+export default function BasicButtons() {
   return (
-    <>
-      <Button variant="contained">Button 1</Button>
-      <Button variant="outlined">Button 2</Button>
-    </>
+    <Stack spacing={2} direction="row">
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </Stack>
   );
 }
-
-export default ButtonBasic;
