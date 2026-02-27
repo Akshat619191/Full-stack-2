4cb830c1fdb791f633ec30cc2a2d8df670239879
Akshat Sharma <akshatsharma@Akshats-MacBook-Air.local>
2026-02-27 18:08:35 +0530

    Added Experiment 8 Flask REST API

---
+++ Backend/exp-8/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+
+    return app
+
+app = create_app()
+
+@app.route("/")
+def home():
+    return {"message": "Backend Server is running"}
